530ec394db7441513004e610862704f29853e0f8
fujitsu4 <zakaria.jouilil@gmail.com>
2025-11-22 20:14:01 +0100

    fix utils.path

--- src/utils/paths.py
+++ src/utils/paths.py
@@ -7,5 +7,5 @@ def get_project_path(*subpath):
     Usage:
         get_project_path("logs", "rejected_snli.txt")
     """
-    root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
+    root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
     return os.path.join(root, *subpath)
